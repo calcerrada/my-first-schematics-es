9583a9a8f293346e526e878fde99b8a798d632ce
Maikel <mariocalcerrada@gmail.com>
2018-08-13 22:57:09 +0200

    Update dependencies and some minor fixes

--- test-schematics/src/app/app.module.ts
+++ test-schematics/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { MyFirstSchemaComponent } from './my-schema/my-first-schema.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyFirstSchemaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
